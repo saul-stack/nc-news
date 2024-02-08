00a49487ae04ab1cf5ade7cd34ade0e341f40fa9
Saul <125879463+saul-stack@users.noreply.github.com>
2024-02-08 22:31:30 +0000

    remove comments for completed task

--- src/components/DummyComment.jsx
+++ src/components/DummyComment.jsx
@@ -1,10 +1,5 @@
 export default function DummyComment({ comment, deleteDummy, index }) {
   const { userName, body, comment_id } = comment;
-
-  //need to somehow pass in the comment_id from the new comment.
-  // will need get request to  server and refresh the comments?
-  //this comment_id will then be sent as a delete request to the server
-  //currently, deleting dummy_comment does not send any delete request to the server
   console.log(comment_id);
   return (
     <section className="comment-card">
